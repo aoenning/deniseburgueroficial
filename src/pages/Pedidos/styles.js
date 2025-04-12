6579f4e8d64785cd1b4c0454165fe9588df4294b
Anderson Oenning <anderson.oenning@amaggi.com.br>
2025-04-12 08:47:35 -0400

    Versão v10

--- src/pages/Pedidos/styles.js
+++ src/pages/Pedidos/styles.js
@@ -15,13 +15,13 @@ export const Container = styled.div`
 `;
 
 export const Header = styled.div`
-  /* display: flex; */
-  /* flex-direction: column; */
-  /* justify-content: center; */
-  /* align-items: center; */
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
   /* position: relative; */
   width: 100%;
-  /* height: 60px; */
+  height: 60px;
   background: ${colors.brack};
 `;
 
